f4516aa41c8df407b7c54ef5c517a5d06a8fa437
BetaKrankri <edominguezj@gmail.com>
2023-07-09 12:55:28 -0600

    chore: triangular calcs. test

--- test/calculus.test.ts
+++ test/calculus.test.ts
@@ -14,15 +14,41 @@ describe("Calculus.triangular()", () => {
     // V
     assert.strictEqual(result, expected);
   });
+
+  it("La suma de dos numeros triangulares iguales da un numero oblongo", () => {
+    // S
+    const input = 10;
+    const expected = input * (input + 1);
+    // E
+    const result = 2 * Calculus.triangular(input);
+    // V
+    assert.strictEqual(result, expected);
+  });
+
+  it("La suma de los primeros numeros triangulares da un numero tetraedrico", () => {
+    // S
+    const input = 10;
+    const expected = (input * (input + 2) * (input + 1)) / 6;
+    // E
+    let result = 0;
+    for (let i = 0; i <= input; i++) {
+      result += Calculus.triangular(i);
+    }
+    // V
+    assert.strictEqual(result, expected);
+  });
 });
 
 /// Fibonacci
 describe("Calculus.fibonacci()", () => {
   it("Deberia satisfacer la formula de Binet", () => {
     // S
+    const input = 40;
     const aurus = (1 + Math.sqrt(5)) / 2;
-    const input = 8;
-    const expected = ((aurus ^ input) - ((1 / -aurus) ^ input)) / Math.sqrt(5);
+    const expected = Math.floor(
+      //formula de binet
+      (aurus ** input - (1 / -aurus) ** input) / Math.sqrt(5)
+    );
     // E
     const result = Calculus.fibonacci(input);
     // V
