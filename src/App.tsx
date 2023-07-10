89563b9318b714cc3242ac884ae8f6372dbf7e6c
BetaKrankri <edominguezj@gmail.com>
2023-07-10 13:13:28 -0600

    comp: resultDisplay

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import "./styles/App.css";
+import ResultDisplay from "./components/ResultDisplay.tsx";
+import Form from "./components/Form.tsx";
 
 function App() {
-  return <>Application</>;
+  return (
+    <div className="App flex items-center justify-center w-full h-screen ">
+      <div className="SeriesCalculator flex flex-col items-center gap-7 p-10 bg-[#132020] rounded-md">
+        <ResultDisplay />
+        <Form />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ResultDisplay.tsx
@@ -0,0 +1,15 @@
+type Props = {};
+
+export default function ResultDisplay({}: Props) {
+  return (
+    <div className="ResultDisplay w-64 h-full rounded-md bg-[#5FA0A0] px-4 py-5 text-lg text-[#0A1010]">
+      <p>
+        S(n) <span className="text-[#864C13]">=</span>
+      </p>
+      <p>
+        P(n+3)*T(n-1)/F(n-2) <span className="text-[#864C13]">=</span>
+      </p>
+      <p className="text-[#2F4F4F] font-bold text-xl">Infinity</p>
+    </div>
+  );
+}
