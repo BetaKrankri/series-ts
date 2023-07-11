57527ca36704bd685dcf4391aa0d94b21e3ffa76
BetaKrankri <edominguezj@gmail.com>
2023-07-10 18:12:42 -0600

    chore: add jsDoc comments

--- src/Calculus.ts
+++ src/Calculus.ts
@@ -1,3 +1,6 @@
+/**
+ * Enumeracion de las series disponibles
+ */
 export enum SERIES {
   ser = "Crazy Serie",
   pri = "Primos",
@@ -5,12 +8,25 @@ export enum SERIES {
   fib = "Fibonacci",
 }
 
+/**
+ * CLase que contiene métodos para calculos de series.
+ */
 class Calculus {
+  /**
+   * Calcula el número triangular para un número dado.
+   * @param n - El número para el cual se calculará el número triangular.
+   * @returns El número triangular calculado.
+   */
   static triangular(n: number): number {
     if (n <= 0) return 0;
     return (n * (n + 1)) / 2;
   }
 
+  /**
+   * Calcula el número de Fibonacci para un número dado.
+   * @param n - El número para el cual se calculará el número de Fibonacci.
+   * @returns El número de Fibonacci calculado.
+   */
   static fibonacci(n: number): number {
     // if (n <= 0) return 0;
     const aurus = (1 + Math.sqrt(5)) / 2;
@@ -19,6 +35,11 @@ class Calculus {
     return fib;
   }
 
+  /**
+   * Calcula el n-ésimo número primo.
+   * @param n - El índice del número primo a calcular.
+   * @returns El n-ésimo número primo calculado.
+   */
   static prime(n: number): number {
     if (n <= 0) return 0;
     const primes: number[] = [];
@@ -36,6 +57,11 @@ class Calculus {
     return primes[primes.length - 1];
   }
 
+  /**
+   * Calcula el valor de la serie para un número dado.
+   * @param n - El número para el cual se calculará el valor de la serie.
+   * @returns El valor de la serie calculado.
+   */
   static serie(n: number): number {
     const P = this.prime(n + 3);
     const T = this.triangular(n - 1);
